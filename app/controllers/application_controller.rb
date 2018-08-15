33f1e498f9a04f238b7ed1d6ac762039e5ded97f
Jon Wenger <jonwenger783@gmail.com>
2018-08-15 11:18:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,28 @@
-class ApplicationController < Sinatra::Base
 
-  configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
-  end
 
-  get "/" do
-  	erb :index
+
+
+
+
+
+
+
+
+
+
+
+
+require_relative '../config/environment'
+
+class App < Sinatra::Base
+
+fortunes = ["eating shellfish", "making code that doesn't work"]
+  get '/' do
+    erb :fortune
   end
+  
+  post '/tell_fortune' do
+    "Oh my dear #{params[:name]}.Unfortunatelyl, you, like all #{params[:sign]} are destined to a fate of #{fortunes.sample}"
+  end 
+  
 end
